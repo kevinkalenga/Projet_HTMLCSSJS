8a195fd34dc1d8b0cc4f8345ac6c282b672bb1ff
kevinkalenga <kevinkalenga10@gmail.com>
2023-04-11 14:07:06 +0200

    footer et bouton retourHaut

--- app.js
+++ app.js
@@ -2,10 +2,13 @@ const burger = document.querySelector(".burger");
 const nav = document.querySelector("nav");
 const liens = document.querySelectorAll(".lien-navigation");
 const navPrincipale = document.querySelectorAll(".nav-principale");
+const retourHaut = document.querySelector(".retourHaut");
+let hauteurAvantNav = nav.offsetTop;
 console.log(burger);
 console.log(nav);
 console.log(liens);
 console.log(navPrincipale);
+console.log(retourHaut);
 
 burger.addEventListener("click", () => {
   burger.classList.toggle("active");
@@ -17,6 +20,12 @@ const changeFond = () => {
     nav.classList.add("font-scroll");
   } else {
     nav.classList.remove("font-scroll");
+  }
+  //Retourhaut
+  if (window.scrollY >= 500) {
+    retourHaut.classList.add("active");
+  } else {
+    retourHaut.classList.remove("active");
   }
 };
 window.addEventListener("scroll", changeFond);
